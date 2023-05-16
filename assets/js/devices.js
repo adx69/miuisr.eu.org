567b3c39b63366893741ebaf38e6f46c55fb11b8
Aditya <ar6553@gmail.com>
2023-05-16 19:18:24 +0530

    d

---
+++ assets/js/devices.js
@@ -0,0 +1,20 @@
+TweenMax.staggerTo(
+    ".devices",
+    0.8,
+    {
+        delay: 0,
+        opacity: 1,
+        ease: Power4.easeInOut,
+    },
+    0.2
+);
+TweenMax.staggerTo(
+    ".btn",
+    3,
+    {
+        delay: 0.1,
+        opacity: 1,
+        ease: Power4.easeInOut,
+    },
+    0.15
+);
---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+TweenMax.to(
+    ".home",
+    0.5,
+    {
+        delay: 1.3,
+        opacity: "1",
+        ease: Power4.easeInOut,
+    },
+    1
+);
+
+TweenMax.to(
+    ".btn",
+    0.5,
+    {
+        delay: 1.4,
+        opacity: "1",
+        ease: Power4.easeInOut,
+    },
+    1
+);
